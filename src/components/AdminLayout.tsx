b0b856606f0b1f73f5d4486f5b61c1142b31198d
Enzo Pitana <enzopitana@gmail.com>
2026-04-22 21:47:43 -0300

    Update AdminLayout.tsx

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -163,4 +163,12 @@ export default function AdminLayout() {
         </nav>
 
         {/* Page content */}
-        <main className="flex-1 ove
+        <main className="flex-1 overflow-auto">
+          <div className="mx-auto max-w-6xl p-4 sm:p-6 lg:p-8">
+            <Outlet />
+          </div>
+        </main>
+      </div>
+    </div>
+  )
+}
